d2f698ca5db7eac11dd62fc524fe41921d1ad191
KayongoYongo <sknsammie10@gmail.com>
2024-03-19 03:15:27 +0300

    added a script for adding a task

--- addTaskTest.py
+++ addTaskTest.py
@@ -1,18 +1,22 @@
-from db import DB
+from models import storage
 from models.tasks import Tasks  # Import the Tasks model
-
-# Create an instance of the DB class
-db = DB()
 
 # Define task data
 task_data = {
-    'title': 'Complete project',
-    'description': 'Finish coding the project',
+    'title': 'Test project',
+    'description': 'Finish fixing the project',
     'status': 'todo'  # Note: Assuming status can only be 'todo' or 'done'
 }
 
 # Add a task using the add_task method
-new_task = db.add_task(**task_data)
+new_task = Tasks(**task_data)
+
+# Use the storage instance to interact with the database
+# Add the new task to the session
+storage.new(new_task)
+
+# Commit the transaction
+storage.save() 
 
 # Print the newly created task
 print("New Task:")
