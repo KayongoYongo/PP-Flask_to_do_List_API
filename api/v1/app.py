ac31fa0a063ddc6807044472da77a48fbb9cc59a
KayongoYongo <sknsammie10@gmail.com>
2024-03-19 03:06:13 +0300

    updated app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,5 @@
-from flask import Flask
+from flask import Flask, jsonify
 from api.v1.views import app_views
-import sys
 
 """
 This line tells flask to use the module 
@@ -10,8 +9,6 @@ like templates and static files
 app = Flask(__name__)
 
 app.register_blueprint(app_views)
-
-print(sys.path)
 
 if __name__ == "__main__":
     app.run(debug=True)
